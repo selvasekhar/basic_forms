50b74d925a2e493ffd1e639d1437ad0c47dcc948
selvasekhar <selvasekhar@gmail.com>
2022-11-03 22:25:29 +0530

    basiforms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { User } from './user';
+import {EnrollmentService} from './enrollment.service'
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-project';
+  // title = 'my-first-project';
+  topics = ['Angular', 'type', 'js'];
+  topicHasError=true;
+  submitted = false;
+  errorMsg = '';
+  userForm : any
+
+  userModel = new User ('selva', 'selvasekar@hotmail.com', '9841188153', '', 'morning', true)
+  
+  constructor(private _enrollmentService:EnrollmentService ){}
+
+  validdateTopic(value: string){
+    if(value === 'default'){
+      this.topicHasError = true;
+    }else{
+      this.topicHasError = false
+    }
+  }
+
+  onSubmit(userForm: any){
+    console.log(userForm);
+//     this.submitted=true;
+//  this._enrollmentService.enroll(this.userModel)
+//  .subscribe(
+//     data => console.log('Success' , data),
+//     error => this.errorMsg = error.statusText
+//  )
+  }
 }
+ 
